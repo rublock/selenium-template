90c868132c8c7dea526db88fee9252825b036fc8
Mack <hypermail@yandex.ru>
2025-03-09 20:56:52 +0300

    test_count_products_on_page

--- test_main.py
+++ test_main.py
@@ -2,10 +2,15 @@ import time
 
 from selenium.webdriver.common.by import By
 
-link = "https://termoshkaf.ru/"
+link = "https://www.advantageonlineshopping.com/#/category/Laptops/1"
 
 
-def test_guest_should_see_login_link(browser):
+def test_count_products_on_page(browser):
     browser.get(link)
-    browser.find_element(By.XPATH, "//span[text()='Каталог']").click()
-    time.sleep(5)
+    browser.implicitly_wait(10)
+    review_block = browser.find_element(
+        By.XPATH, "/html/body/div[3]/section/article/div[3]/div/div/div[2]/ul"
+    )
+    reviews = review_block.find_elements(By.XPATH, "./*")
+    time.sleep(5)  # human verify
+    assert len(reviews) == 11
